21137f26bd83757b9349cc911dd821991ad69919
tkb16 <marc.brugger@six-group.com>
2017-10-30 15:59:38 +0100

    initial commit

---
+++ jenkins/BaseFunctions.groovy
@@ -0,0 +1,19 @@
+package com.six_group.jenkins.pipeline
+
+class Constants {
+    static final String DATABASE = "DCBO20"
+    static final String BITBUCKET_URL = "test-stash01.dev.six-group.net"
+    static final String PASSENT3_LOCAL_M2_PATH = "/backup/tmp/pass-branch-maven-repos/"
+
+    static final String BUILD_PASS_PREFIX = "BUILD_PASS_"
+    static final String BUILD_IPASS_PREFIX = "BUILD_IPASS_"
+    static final String FUNC_PASS_PREFIX = "FUNC_PASS_"
+    static final String FUNC_IPASS_PREFIX = "FUNC_IPASS_"
+    static final String TEST_PASS_PREFIX = "TEST_PASS_"
+    static final String TEST_IPASS_PREFIX = "TEST_IPASS_"
+    static final String CIT_PASS_PREFIX = "CIT_PASS_"
+    static final String CIT_IPASS_PREFIX = "CIT_IPASS_"
+
+    static final String BUILD_PASS_RELEASE = "BUILD_PASS"
+    static final String BUILD_IPASS_RELEASE = "BUILD_IPASS"
+}
