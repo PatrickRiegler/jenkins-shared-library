1c26d81ff6f6c8e137f033a23137cb7d46942b76
tkb16 <marc.brugger@six-group.com>
2017-10-30 17:12:37 +0100

    add dockerToken function

--- src/com/six_group/jenkins/pipeline/dockerToken.groovy
+++ src/com/six_group/jenkins/pipeline/dockerToken.groovy
@@ -1,4 +1,4 @@
-def dockerToken(String login = "serviceaccount") {
+def call(String login = "serviceaccount") {
     node() {
         // Read the auth token from the file defined in the env variable AUTH_TOKEN
         String token = sh returnStdout: true, script: 'cat ${AUTH_TOKEN}'
