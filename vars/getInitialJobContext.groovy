e72c82de8c6d280faab863bb550ba71ee75147f6
tkb16 <marc.brugger@six-group.com>
2017-10-31 07:52:41 +0100

    remove stage

--- vars/getInitialJobContext.groovy
+++ vars/getInitialJobContext.groovy
@@ -2,13 +2,11 @@ def call(boolean withVersion = true) {
     def jobContext = [:]
 
     node() {
-        stage("Setup") {
-            if (withVersion) {
-                // generate version number
-                jobContext.currentBuildVersion = sh(returnStdout: true, script: 'date +%Y%m%d%H%M%S  -u').trim()
-            }
-            echo "${jobContext}"
+        if (withVersion) {
+            // generate version number
+            jobContext.currentBuildVersion = sh(returnStdout: true, script: 'date +%Y%m%d%H%M%S  -u').trim()
         }
+        echo "${jobContext}"
     }
     return jobContext
 }
